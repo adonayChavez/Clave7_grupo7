1bc252b975270de2879bbee01685092de51529d8
AdonayChavez <chavezadonay3@gmail.com>
2025-05-17 23:20:46 -0600

    Metodo CrearTarea

--- Program.cs
+++ Program.cs
@@ -19,66 +19,52 @@ class Program
     public static void Main(string[] args)
     {
 
-        String nombreUsuario;
-        String id;
-        String correo;
+        List<Tarea> tareas = new List<Tarea>();
 
-
-        //validar nombre
-        /*do
-        {
-            Console.WriteLine("Ingresa tu nombre");
-            nombreUsuario = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(nombreUsuario))
-                Console.WriteLine("Error el nombre no puede estar vacio. Intenta de nuevo.");
-        } while (string.IsNullOrWhiteSpace(nombreUsuario));
-
-        
-        Console.WriteLine("Ingresa tu id");
-        id = Console.ReadLine();
-        */
-
-
-        Console.WriteLine("""
+        //menu
+        while (true) {
+            Console.WriteLine("""
+                
+                ----------MENU PRINCIPAL-----------
                 1. Crear nueva tarea.
                 2. Editar tarea.
                 3. Ordenar tareas.
                 4. Mostrar tareas proximas a vencer 
-                5. Marcar tarea como completadas
+                5. Marcar tarea como completada
                 6. Salir
-
+                -----------------------------------
+                
                 """);
 
-        Console.WriteLine("Ingresa opcion ");
-        var opcion = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa opcion ");
+            var opcion = int.Parse(Console.ReadLine());
 
-        switch (opcion)
-        {
-
-            case 1:
-                crearNuevaTarea();
-                break;
-            case 2:
-                editarTarea();
-                break;
-            case 3:
-                ordenarTareas();
-                break;
-            case 4:
-                MostrarTareasAVencer();
-                break;
-            case 5:
-                MarcarTareaComoCompletada();
-                break;
-            case 6:
-                Console.WriteLine("Presiona cualquier tecla para cerrar programa......");
-                Console.ReadKey();
-                break;
-
-            default:
-                Console.WriteLine("Opcion no valida");
-                break;
+            switch (opcion)
+            {
+                case 1:
+                    crearNuevaTarea(tareas);
+                    break;
+                case 2:
+                    editarTarea();
+                    break;
+                case 3:
+                    ordenarTareas();
+                    break;
+                case 4:
+                    MostrarTareasAVencer();
+                    break;
+                case 5:
+                    MarcarTareaComoCompletada();
+                    break;
+                case 6:
+                    Console.WriteLine("Programa finalizado");
+                    return;
+                default:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+            }
         }
+        
 
     }
 
@@ -102,8 +88,28 @@ class Program
         throw new NotImplementedException();
     }
 
-    private static void crearNuevaTarea()
+    private static void crearNuevaTarea(List<Tarea> listaTareas)
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Descripción de la tarea: ");
+        string descripcion = Console.ReadLine();
+
+        Console.WriteLine("fecha de vencimiento: ");
+        DateTime fechaVencimiento = DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingresa tu nombre de usuario:");
+        string creador = Console.ReadLine();
+
+        Tarea tareaNueva = new Tarea
+        {
+            Descripcion = descripcion,
+            FechaDeVencimiento = fechaVencimiento,
+            Completada = false,
+            CreadoPor = creador
+        };
+        listaTareas.Add(tareaNueva);
+        Console.WriteLine("Tarea agragada con exito");
+        Console.WriteLine("Presiona cualquier tecla para volver al menu principal");
+        Console.ReadKey();
+
     }
-}
+} 
--- obj/Release/net8.0/Clave7_grupo7.AssemblyInfo.cs
+++ obj/Release/net8.0/Clave7_grupo7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clave7_grupo7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032709f95eec103b5adc5e3cd3bbad89854b690e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clave7_grupo7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clave7_grupo7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
