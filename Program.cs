14b9d78329c36696f94fcd643df81374e9465f5f
AdonayChavez <chavezadonay3@gmail.com>
2025-06-05 15:38:56 -0600

    Arreglos esteticos 01

--- Program.cs
+++ Program.cs
@@ -36,12 +36,12 @@ class Program
 
     public static void Main(string[] args)
     {
-        Console.WriteLine("----------Bienvendio a nuestro programa de gestion de tareas----------");
-        Console.WriteLine("A continuacion debes registrar tus datos");
+        Console.WriteLine("----------Bienvenido a nuestro programa de gestión de tareas----------");
+        Console.WriteLine("\nA continuación debes registrar tus datos");
         usuario = RegistrarUsuario();
         Console.Clear();
         Console.WriteLine($"Nombre: {usuario.NombreUsuario}\nDUI o Carnet: {usuario.NumeroIdentificacion}\nCorreo: {usuario.CorreoElectronico}");
-        Console.WriteLine("Presiona cualquier tecla para continuar al menú...");
+        Console.WriteLine("\nPresiona cualquier tecla para continuar al menú...");
         Console.ReadKey();
 
         //menu
@@ -49,7 +49,7 @@ class Program
             Console.Clear();
             Console.WriteLine("""
                 
-                ----------MENU PRINCIPAL-----------
+                ----------MENÚ PRINCIPAL-----------
                 1. Crear nueva tarea.
                 2. Editar tarea.
                 3. Ordenar tareas por fecha y estado. 
@@ -59,7 +59,7 @@ class Program
                 
                 """);
 
-            Console.WriteLine("Ingresa opcion ");
+            Console.WriteLine("Ingresa opción ");
             var opcion = int.Parse(Console.ReadLine());
             Console.Clear();
             switch (opcion)
@@ -80,7 +80,7 @@ class Program
                     Console.WriteLine("Programa finalizado");
                     return;
                 default:
-                    Console.WriteLine("Opcion no valida");
+                    Console.WriteLine("Opción no válida");
                     break;
             }
         }
@@ -97,20 +97,21 @@ class Program
         //asegurar que el arreglo de tareas no este vacía
         if (listaTareas == null || listaTareas.Length == 0)
         {
-            Console.WriteLine("(!) La lista de tareas está vacia. Intenta agregar una tarea antes");
-            Console.Write("Presiona cualquier tecla para volver al menu principal y agregar una tarea");
+            Console.WriteLine("(!) La lista de tareas está vacía. Intenta agregar una tarea antes");
+            Console.Write("Presiona cualquier tecla para volver al menú principal y agregar una tarea");
             Console.ReadKey();
             return;
 
         }
             //mostrarle la lista de tareas al usuario para que sepa mejor cual quiere marcar
-            Console.WriteLine("-------------------------Lista de tareas------------------------");
+            Console.WriteLine("-------------------------Lista de tareas------------------------\n");
             for (int i = 0; i < listaTareas.Length; i++)
             {
                 Console.WriteLine($"""
+                -------------------------------------------------------------
                 Tarea {i + 1}
                 Descripción: {listaTareas[i].Descripcion}
-                Vence: {listaTareas[i].FechaDeVencimiento}
+                Vence: {listaTareas[i].FechaDeVencimiento.ToShortDateString()}
                 Estado: {listaTareas[i].Estado}
                 ---------------------------------------------------------------
                 """);
@@ -119,11 +120,11 @@ class Program
             //sacar el dato de la trea a modificar 
             do
             {
-                Console.WriteLine("Ingresa el numero de tarea a la cual quieres marcar como completada");
+                Console.WriteLine("\nIngresa el número de tarea a la cual quieres marcar como completada");
                 entradaValida = int.TryParse(Console.ReadLine(), out numeroDeTarea);
                 if (!entradaValida || numeroDeTarea <= 0 || numeroDeTarea > listaTareas.Length)
                 {
-                    Console.WriteLine("(!) Valor invalido. Ingrese un numero de tarea valido");
+                    Console.WriteLine("(!) Valor inválido. Ingrese un número de tarea válido");
                 }
             }
             while (!entradaValida || numeroDeTarea <= 0 || numeroDeTarea > listaTareas.Length);
@@ -132,7 +133,7 @@ class Program
             if (listaTareas[numeroDeTarea - 1].Estado == EstadoTarea.Completada)
             {
                 Console.WriteLine("Esa tarea ya ha sido marcada como completada");
-                Console.Write("Presiona cualquier tecla para volver al menu principal");
+                Console.Write("Presiona cualquier tecla para volver al menú principal");
                 Console.ReadKey();
                 return;
 
@@ -140,8 +141,18 @@ class Program
 
             //modificar el estado como true "Completada"
             listaTareas[numeroDeTarea - 1].Estado = EstadoTarea.Completada;
-            Console.WriteLine("Tarea marcada con éxito");
-            Console.Write("Presiona cualquier tecla para volver al menu principal");
+        Console.Clear();
+        Console.WriteLine("Tarea marcada con éxito\n");
+        MostrarDatosUsuario();
+        Console.WriteLine($"""
+            ----------------------------------------------------------------------------
+            |Tarea: {numeroDeTarea}
+            |Descripción: {listaTareas[numeroDeTarea-1].Descripcion}
+            |Fecha: {listaTareas[numeroDeTarea-1].FechaDeVencimiento.ToShortDateString()}
+            |Estado: {listaTareas[numeroDeTarea-1].Estado}
+            ----------------------------------------------------------------------------
+            """);
+        Console.Write("\nPresiona cualquier tecla para volver al menú principal");
         Console.ReadKey();
     }
     private static void ordenarTareas(Tarea[]listaTareas)
@@ -149,7 +160,7 @@ class Program
         //validar que la lista de tareas no este vacia
         if (listaTareas == null|| listaTareas.Length == 0 )
         {
-            Console.WriteLine("(!) La lista de tareas esta vacia. Intenta agregar una nueva tarea antes");
+            Console.WriteLine("(!) La lista de tareas esta vacía. Intenta agregar una nueva tarea antes");
             return;
         }
         else Console.WriteLine("-----------------------Lista de tareas ordendas por fecha de vencimiento y Estado---------------------");
@@ -188,31 +199,33 @@ class Program
         Console.WriteLine($"Total de tareas vencidas: {listaTareas.Count(t => t.Estado == EstadoTarea.Vencida)}");
         Console.WriteLine($"Tareas a vencer en los proximos 5 dias: {listaTareas.Count(t => (t.FechaDeVencimiento - DateTime.Today).Days <= 5 && (t.FechaDeVencimiento - DateTime.Today).Days > 0)}");
         Console.WriteLine();
-        Console.Write("Presiona cualquier tecla para volver al menu principal");
+        Console.Write("Presiona cualquier tecla para volver al menú principal");
         Console.ReadKey();
     }
 
     private static void editarTarea()
     {
+        String Nuevadescripcion;
+        DateTime NuevafechaDeVencimiento;
         int tareaAEditar;
         bool entradaValida;
-        //validar que la lista de tareas no este vacia
+        //válidar que la lista de tareas no este vacía
         if (listaTareas == null || listaTareas.Length == 0)
         {
-            Console.WriteLine("(!) La lista de tareas esta vacia. Intenta agregar una nueva tarea antes");
+            Console.WriteLine("(!) La lista de tareas está vacía. Intenta agregar una nueva tarea antes");
             return;
         }
         //mostrar al usuario la lista de tareas
-        Console.WriteLine("-----------------------Lista de tareas---------------------");
+        Console.WriteLine("-----------------------------Lista de tareas----------------------------");
         //recorrer y mostrar al usuario toda la lista de tareas
         for (int i = 0; i < listaTareas.Length; i++)
         {
             Console.WriteLine($"""
-                
+                ------------------------------------------------------------------------
                 |Tarea {i + 1} 
-                |Descripcion: {listaTareas[i].Descripcion}
+                |Descripción: {listaTareas[i].Descripcion}
                 |Vence: {listaTareas[i].FechaDeVencimiento.ToShortDateString()}
-                |sEstado: {(listaTareas[i].Estado)}
+                |Estado: [{listaTareas[i].Estado}]
                 ------------------------------------------------------------------------
                 """);
                 
@@ -220,38 +233,39 @@ class Program
        
         do
         {
-            Console.Write("Ingresa el numero de la tarea que quieres editar: ");
+            Console.Write("Ingresa el número de la tarea que quieres editar: ");
             entradaValida = int.TryParse(Console.ReadLine(), out tareaAEditar);
             if (!entradaValida || tareaAEditar < 1 || tareaAEditar > listaTareas.Length)
             {
-                Console.WriteLine("(!) Debes ingresar un numero valido");
+                Console.WriteLine("(!) Debes ingresar un número válido");
             }
         }
         while (!entradaValida || tareaAEditar < 1 || tareaAEditar > listaTareas.Length);
 
         tareaAEditar--; // Ajustar el índice para que coincida con el arreglo
-        Console.WriteLine($"*****Editando tarea {tareaAEditar + 1}:*****");
+
+        Console.Clear();
+        Console.WriteLine($"********Editando tarea {tareaAEditar + 1}:********\n");
         //validacion para la descripcion
-        String Nuevadescripcion;
+        
         do
         {
-            Console.Write("Descripcion: ");
+            Console.Write("Nueva Descripción: ");
             Nuevadescripcion = Console.ReadLine();
-            if (Nuevadescripcion == "" || Nuevadescripcion == " ")
+            if (Nuevadescripcion == null || Nuevadescripcion == " ")
             {
-                Console.WriteLine("(!) La descripcion de la tarea no puede estar vacia.");
+                Console.WriteLine("(!) La descripción de la tarea no puede estar vacía.");
             }
         }
         while (Nuevadescripcion == "" || Nuevadescripcion == " ");
 
-        DateTime NuevafechaDeVencimiento;
         do
         {
-            Console.Write("Fecha de vencimiento (YYYY-MM-DD): ");
+            Console.Write("Nueva fecha de vencimiento (YYYY-MM-DD): ");
             entradaValida = DateTime.TryParse(Console.ReadLine(), out NuevafechaDeVencimiento);
             if (!entradaValida)
             {
-                Console.WriteLine("(!) Fecha invalida. Intenta nuevamente.");
+                Console.WriteLine("(!) Fecha inválida. Intenta nuevamente.");
             }
         }
         while (!entradaValida);
@@ -260,9 +274,21 @@ class Program
         listaTareas[tareaAEditar].Descripcion = Nuevadescripcion;
         listaTareas[tareaAEditar].FechaDeVencimiento = NuevafechaDeVencimiento;
         listaTareas[tareaAEditar].Estado = NuevafechaDeVencimiento < DateTime.Today ? EstadoTarea.Vencida: EstadoTarea.EnProceso; // Reiniciar el estado a "En proceso"
-        Console.WriteLine("Tarea editada con exito");
-        Console.WriteLine();
-        Console.Write("Presiona cualquier tecla para volver al menu principal");
+
+        Console.Clear();
+        Console.WriteLine("Tarea editada con éxito\n");
+        MostrarDatosUsuario();
+
+        Console.WriteLine($"""
+            ----------------------------------------------------------------------------------------
+            |Tarea {tareaAEditar+1}
+            |Descripción: {listaTareas[tareaAEditar].Descripcion}
+            |Fecha de Vencimiento: {listaTareas[tareaAEditar].FechaDeVencimiento.ToShortDateString()}
+            |Estado: [{listaTareas[tareaAEditar].Estado}]
+            -----------------------------------------------------------------------------------------
+            """);
+        
+        Console.Write("\nPresiona cualquier tecla para volver al menú principal");
         Console.ReadKey();
     }
 
@@ -275,17 +301,17 @@ class Program
 
         do
         {
-            Console.Write("¿Cuantas tareas quieres agregar esta vez? ");
+            Console.Write("¿Cuántas tareas quieres agregar esta vez? ");
             entradaValida = int.TryParse(Console.ReadLine(), out nuevasTareas);
 
             if (!entradaValida||nuevasTareas<=0)
             {
-                Console.WriteLine("(!) Debes ingresar un valor valido");
+                Console.WriteLine("(!) Debes ingresar un valor válido");
             }
         }
         while (!entradaValida||nuevasTareas<=0);
 
-        //creacion de nuevo arreglo con espacio 
+        //creación de nuevo arreglo con espacio 
         Tarea[] nuevoArreglo = new Tarea[cantidadDeTareas + nuevasTareas];
         //copiar las tareas existentes al nuevo arreglo
         for (int i = 0; i < cantidadDeTareas; i++)
@@ -296,26 +322,27 @@ class Program
         //agregar nuevas tareas
         for (int i = cantidadDeTareas; i < cantidadDeTareas + nuevasTareas; i++)
         {
-            Console.WriteLine($"*****ingresando tarea {i + 1}:*****");
+            Console.Clear();
+            Console.WriteLine($"\n*****ingresando tarea {i + 1}:*****");
             //validacion para la descripcion
             do
             {
-                Console.Write("Descripccion: ");
+                Console.Write("Descripcción: ");
                 descripcion=Console.ReadLine();
                 if (descripcion==""||descripcion==" ")
                 {
-                    Console.WriteLine("(!) La descripcion de la tarea no puede estar vacia.");
+                    Console.WriteLine("(!) La descripción de la tarea no puede estar vacía.");
                 }
             }
             while (descripcion==""||descripcion==" ");
 
             do
             {
-                Console.Write("Fecha de vencimeinto (YYYY-MM.DD):");
+                Console.Write("Fecha de vencimiento (YYYY-MM.DD):");
                 entradaValida=DateTime.TryParse(Console.ReadLine(), out fechaDeVencimiento);
                 if (!entradaValida)
                 {
-                    Console.WriteLine("(!) Fecha invalida. intenta nuevamente.");
+                    Console.WriteLine("(!) Fecha inválida. Intenta nuevamente.");
                 }
             }
             while (!entradaValida);
@@ -328,13 +355,33 @@ class Program
             };
 
         }
-        //actualizar el arreglo y el contador de tareas
-        listaTareas = nuevoArreglo;
-        cantidadDeTareas += nuevasTareas;
 
-        Console.WriteLine("Todas las tareas han sido agregadas con exito");
-        Console.WriteLine();
-        Console.Write("Presiona cualquier tecla para volver al menu principal");
+        //actualizar arreglo
+        listaTareas = nuevoArreglo;
+
+        Console.Clear();
+        Console.WriteLine("\nTodas las tareas han sido agregadas con éxito\n");
+        MostrarDatosUsuario();
+
+        int inicioNuevasTareas = cantidadDeTareas;
+
+        for (int i = inicioNuevasTareas; i < inicioNuevasTareas + nuevasTareas; i++)
+        {
+            Console.WriteLine($"""
+                ----------------------------------------------------------------
+                |Tarea {i-inicioNuevasTareas+1}
+                |Descripción: {listaTareas[i].Descripcion}
+                |Fecha de Vencimiento: {listaTareas[i].FechaDeVencimiento.ToShortDateString()}
+                |Estado: [{listaTareas[i].Estado}]
+                ----------------------------------------------------------------
+                """);
+        }
+        //actualizar el contador de tareas
+        
+        cantidadDeTareas += nuevasTareas;
+      
+        
+        Console.Write("\nPresiona cualquier tecla para volver al menú principal");
         Console.ReadKey();
     }
     static Usuario RegistrarUsuario()
@@ -350,7 +397,7 @@ class Program
 
             if (nombreUsuario == null || nombreUsuario.Length == 0)
             {
-                Console.WriteLine("Error el nombre no puede estar vacio. Inténtelo de nuevo.");
+                Console.WriteLine("Error el nombre no puede estar vació. Inténtelo de nuevo.");
                 valido = false;
             }
             else if (nombreUsuario.Length < 3)
@@ -371,12 +418,12 @@ class Program
 
             if (numeroIdentificacion == null || numeroIdentificacion.Length == 0)
             {
-                Console.WriteLine("Error el DUI no puede estar vacio. Inténtelo de nuevo.");
+                Console.WriteLine("Error el DUI no puede estar vació. Inténtelo de nuevo.");
                 valido = false;
             }
             else if (numeroIdentificacion.Length != 9)
             {
-                Console.WriteLine("Error el DUI debe tener exactamente 9 numeros. Inténtelo de nuevo.");
+                Console.WriteLine("Error el DUI debe tener exactamente 9 números. Inténtelo de nuevo.");
                 valido = false;
             }
             else
@@ -386,7 +433,7 @@ class Program
 
                     if (numeroIdentificacion[i] < '0' || numeroIdentificacion[i] > '9')
                     {
-                        Console.WriteLine("Error solo se permiten numeros enteros positivos. Inténtelo de nuevo");
+                        Console.WriteLine("Error solo se permiten números enteros positivos. Inténtelo de nuevo");
                         valido = false;
                         break;
                     }
@@ -397,17 +444,17 @@ class Program
         Console.Clear();
 
 
-        // ingreso y validacion de el correo electronico
+        // ingreso y validación de el correo electronico
         do
 
         {
-            valido = true; // Reiniciar la validez para cada iteración
+            valido = true; // Reiniciar la válidez para cada iteración
 
-            Console.WriteLine("Ingresa su correo electronico:");
+            Console.WriteLine("Ingrese su correo electrónico:");
             correoElectronico = Console.ReadLine();
 
             if (correoElectronico == null || correoElectronico.Length == 0)
-                Console.WriteLine("Error el correo no puede estar vacio. Intente de nuevo.");
+                Console.WriteLine("Error el correo no puede estar vacío. Intente de nuevo.");
             else if (correoElectronico.Length < 10)
             {
                 Console.WriteLine("Error el correo debe tener al menos 10 caracteres. Intente de nuevo.");
@@ -507,9 +554,11 @@ class Program
 
     private static string gestionarVencimiento(DateTime fechaDeVencimiento, EstadoTarea estado)
     {
+        int diasRestantes;
+
         if (estado == EstadoTarea.Completada)
             return "";
-        int diasRestantes = (fechaDeVencimiento - DateTime.Today).Days;
+             diasRestantes = (fechaDeVencimiento - DateTime.Today).Days;
 
         if (diasRestantes > 0)
             return $"(Vence en {diasRestantes} dias)";
